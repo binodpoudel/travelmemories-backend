3117a685678383b91ff7181cc2ad1b4a9f95f10e
unknown <footandshadows@gmail.com>
2023-09-14 21:10:19 +0200

    change database

--- index.js
+++ index.js
@@ -18,11 +18,13 @@ app.use("/user", userRouter);
 /*  Database connection write your userName and Password */
 /*const CONNECTION_URL = 'mongodb+srv://Your UserName:Password@cluster0.4xze8.mongodb.net/mernstack?retryWrites=true&w=majority'; */
 
-const CONNECTION_URL = 'mongodb+srv://binod:binodpoudel@cluster0.4xze8.mongodb.net/mernstack?retryWrites=true&w=majority';
+/*const CONNECTION_URL = 'mongodb+srv://binod:binodpoudel@cluster0.4xze8.mongodb.net/mernstack?retryWrites=true&w=majority'; */
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
+/*mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+  .then(() => */
+  app.listen(PORT, () =>{  console.log(`Server Running on Port: http://localhost:${PORT}`) })
+  /*.catch((error) => console.log(`${error} did not connect`)); */
 
 mongoose.set('useFindAndModify', false);
