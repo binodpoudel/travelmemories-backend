8d984a96a66695a23bfe0de0994107bba96f36aa
unknown <footandshadows@gmail.com>
2023-09-16 13:18:03 +0200

    travel memories backend

--- index.js
+++ index.js
@@ -3,28 +3,30 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from "./routes/user.js";
 
-const app = express();
 
+const app = express();
+dotenv.config();
 app.use(express.json({ limit: '30mb', extended: true }))
 app.use(express.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 /* routes*/
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-/*  Database connection write your userName and Password */
+
+/*  Database connection write your userName and Password or use .env */
 /*const CONNECTION_URL = 'mongodb+srv://Your UserName:Password@cluster0.4xze8.mongodb.net/mernstack?retryWrites=true&w=majority'; */
 
-/*const CONNECTION_URL = 'mongodb+srv://binod:binodpoudel@cluster0.4xze8.mongodb.net/mernstack?retryWrites=true&w=majority'; */
+const CONNECTION_URL = process.env.DATABASE;
 const PORT = process.env.PORT|| 5000;
 
-/*mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-
-  .then(() => */
-  app.listen(PORT, () =>{  console.log(`Server Running on Port: http://localhost:${PORT}`) })
-  /*.catch((error) => console.log(`${error} did not connect`)); */
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
+
